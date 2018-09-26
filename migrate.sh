b971eb7459f4ff4cc8b364a8da402033e48ae0d4
Liu Shangji <liushangji@liushangji-macbook-pro.local>
2018-09-26 15:18:45 +0800

    fix

--- migrate.sh
+++ migrate.sh
@@ -3,7 +3,7 @@
 for i in `ls /migrate/sql`
 do 
   echo "CURRENT DB: $i"
-  $db="db_$i";
-  migrate.linux-amd64 -source file://$i -database "${!db}" --verbose up
+  db=db_$i;
+  migrate.linux-amd64 -source file:///migrate/sql/$i -database "${!db}" --verbose up
 done
 
